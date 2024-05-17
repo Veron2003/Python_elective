1b4e7d07d8da690464562992daad4bd636ff8061
Veron2003 <122771286+Veron2003@users.noreply.github.com>
2024-05-17 14:34:53 +0800

    Add files via upload

--- python_proj.py
+++ python_proj.py
@@ -1,4 +1,27 @@
 import RPi.GPIO as GPIO
-from time import *
+import time
+import requests
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+
+
+def normal():
+    #led display 'Welcome' and 'Select the product'
+    #camera take photo
+    #temperature and humidity sensor take readings
+
+
+def select_product():
+    #led display the price of products selected
+    #if product that is not available is selected, display not availale
+    
+def paymment():
+    #make sure payment is made
+
+
+def dispense():
+   #turn motor a certain way to dispense the product
+
+def send_data():
+    #send data to webpage or app
+    
